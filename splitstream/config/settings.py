c02a1db020c0e837a2d26024da448e8b2716bbf3
Farid Mammadov <faridlmmdv@gmail.com>
2026-08-05 04:12:17 +0200

    Move SECRET_KEY to environment variable for security

--- splitstream/config/settings.py
+++ splitstream/config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!y6yi@^9)9*8et1@)3kipv==!!%5vbm&5%07&d91&y0=d12%7f'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-only-key-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
